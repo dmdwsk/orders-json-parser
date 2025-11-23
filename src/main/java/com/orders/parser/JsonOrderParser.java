12e7e5f3b464951eb6bf703e085126db8ad96ce1
dmdwsk <dmdwsk200@gmail.com>
2025-11-24 00:29:47 +0100

    Implement multithreaded JSON parser with FileReaderTask and executor logic

--- src/main/java/com/orders/parser/JsonOrderParser.java
+++ src/main/java/com/orders/parser/JsonOrderParser.java
@@ -3,8 +3,17 @@ package com.orders.parser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.orders.entities.Order;
 
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 public class JsonOrderParser {
     public final ObjectMapper mapper;
@@ -12,7 +21,40 @@ public class JsonOrderParser {
     public JsonOrderParser(ObjectMapper mapper) {
         this.mapper = mapper;
     }
-    public List<Order> parse(String folderPath, int threadCount){
+    public List<Order> parse(String folderPath, int threadCount) {
+        Path folder = Path.of(folderPath);
+        if (!Files.exists(folder) || !Files.isDirectory(folder)) {
+            throw new IllegalArgumentException("Invalid folder path: " + folderPath);
+        }
+
+        List<Path> jsonFiles;
+        try{
+            jsonFiles = Files.list(folder)
+                    .filter(path -> path.getFileName()
+                            .toString().endsWith(".json")).toList();
+        }catch (IOException e){
+           throw new RuntimeException("Failed to read: " + folderPath,e);
+        }
+
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        List<Future<Order>> futures = new ArrayList<>();
+        for(Path file : jsonFiles){
+            FileReaderTask fileReaderTask = new FileReaderTask(file,mapper);
+            futures.add(executor.submit(fileReaderTask));
+        }
+        List<Order> orders = new ArrayList<>();
+        for(Future<Order> future : futures){
+            try {
+                Order order = future.get();
+                if(order != null){
+                    orders.add(order);
+                }
+            }catch (Exception e){
+                System.err.println("Failed to process a file: " + e.getMessage() );
+            }
+        }
+        executor.shutdown();
+        return orders;
 
     }
 }
